ba4bfddd3892d66651a4f4b2c255297f3857d9aa
Sven Pietrowsky <161057908+SvenPietrowsky@users.noreply.github.com>
2024-05-19 18:56:16 +0200

    legalnotice

---
+++ javascript/board.js
@@ -0,0 +1,71 @@
+function openAddTask() {
+    let container = document.getElementById('add-task-popup');
+    container.classList.remove('d-none');
+    container.innerHTML = `
+    <div class="margin-left-top">
+    <h1>Add Task</h1>
+    <div class="add-task-section">
+
+        <div class="add-task-titel-container">
+            <form action="">
+            <p>Titel<span class="color-red">*</span></p>
+            <input required class="margin-buttom" type="text" placeholder="Enter a title">
+            <p>Description</p>
+            <textarea class="margin-buttom" name="" id="" placeholder="Enter a Description"></textarea>
+            <p>Assigned to</p>
+            <div class="custom-select" style="width:100%;">
+                <select>
+                  <option value="0">Select car:</option>
+                  <option class="assigned-conainer" value="1">
+                <div class="assigned-circle-name">SM</div>
+                 <p>Sofia Müller</p>
+                 
+                  </option>
+                </select>
+              </div>
+        </div>
+
+        <div class="add-task-between-line"></div>
+
+        <div class="add-task-date-container">
+            <p>Due date<span class="color-red">*</span></p>
+            <input required class="margin-buttom" type="date">
+            <p>Prio</p>
+            <div class="margin-buttom add-task-prio">
+                <div class="prio-selection-urgent">
+                    <span>Urgent</span> 
+                    <img class="prio-icons" src="./assets/img/add_task/arrowsTop.svg">
+                </div>
+                <div class="prio-selection-medium">
+                    <span>Medium</span> 
+                    <img class="prio-icons" src="./assets/img/add_task/result.svg">
+                </div>
+                <div class="prio-selection-low">
+                    <span>Low</span> 
+                    <img class="prio-icons" src="./assets/img/add_task/arrowsButtom.svg">
+                </div>
+            </div>
+            <p>Category<span class="color-red">*</span></p>
+            <div class="custom-select" style="width:100%;">
+                <select>
+                  <option value="0">Select car:</option>
+                  <option value="1">Audi</option>
+                  <option value="2">BMW</option>
+                </select>
+              </div>
+            <p>Subtasks</p>
+            <input type="text" name="" id="">
+        </div>
+
+    </div>
+
+    <div class="send-add-task-buttons">
+        <p class="required-text"><span class="color-red">*</span>This field is required</p>
+        <div>
+            <button class="clear-button">Clear x</button>
+            <button class="btn" onsubmit="">Create Task ✅</button>
+        </div>
+    </div>
+</form>
+`;
+}
