85f7da12001777e56799ad68e89d81910334eeae
Sven Pietrowsky <161057908+SvenPietrowsky@users.noreply.github.com>
2024-05-20 14:34:01 +0200

    updated popup

--- javascript/contacts.js
+++ javascript/contacts.js
@@ -1,11 +1,28 @@
-function renderContacts() {
+// let contacts = [
+//     {
 
-}
+//     },
+//     {
+
+//     },
+//     {
+
+//     }
+// ];
+
+// function renderContacts() {
+    // let contactcontainer = document.getElementById('contact-container');
+// }
 
 function addNewContact() {
     let container = document.getElementById('add-contact-popup');
     container.classList.remove('d-none');
-    container.innerHTML = `
+    // container.innerHTML = `
     
-    `;
+    // `;
+}
+
+function closePopup() {
+    let container = document.getElementById('add-contact-popup');
+    container.classList.add('d-none');
 }
