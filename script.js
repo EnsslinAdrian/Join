af317558bbadffb95bdacd279dea9d795966042f
EnsslinAdrian <ensslin.adrian@web.de>
2024-06-05 13:36:05 +0200

    chnange load function script.js

--- script.js
+++ script.js
@@ -17,6 +17,11 @@ function init() {
     renderTaskBoard();
 }
 
+async function loadData(path = "") {
+    const response = await fetch(firebaseUrl + ".json");
+    const responseToJson = await response.json();
+}
+
 /**
  * This function checks if the user's login data is correct
  * 
